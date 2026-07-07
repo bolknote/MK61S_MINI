571c03e19877ac4835c047e90cf235491af41a3d
Evgeny Stepanischev <imbolk@gmail.com>
2026-07-07 03:01:27 +0300

    Share language runtime workspace

---
+++ code/language_workspace.hpp
@@ -0,0 +1,21 @@
+#ifndef LANGUAGE_WORKSPACE_HPP
+#define LANGUAGE_WORKSPACE_HPP
+
+#include "rust_types.h"
+
+namespace language_workspace {
+
+enum class Owner : u8 {
+  NONE,
+  FOCAL,
+  TINYBASIC
+};
+
+static constexpr usize SIZE = 8192;
+
+void* acquire(Owner owner, usize required);
+Owner current_owner(void);
+
+} // namespace language_workspace
+
+#endif
